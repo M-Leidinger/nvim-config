caeed920481890da5d49f10a763f97e04fe79d96
M-Leidinger <max.leidinger@kabelmail.de>
2025-12-03 16:55:37 +0100

    configure keybindings for telescope

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -1,16 +1,32 @@
+local builtin = require('telescope.builtin')
+
+-- binds leader key to space
+vim.g.mapleader = " "
+
+-- binds local leader key to double backslash
+vim.g.maplocalleader = "\\"
+
+-- binds <leade>pv to the directory overview
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 -- binds <leader>ff to find_files
 vim.keymap.set(
   "n",
-  "<C-f>",
-  require("telescope.builtin").find_files,
+  "<leader>ff",
+  builtin.find_files,
   { desc = "Telescope: Find Files", silent = true }
 )
 
 -- binds <leader>fg to live_grep
 vim.keymap.set(
   "n",
-  "<C-g>",
-  require("telescope.builtin").live_grep,
+  "<leader>lg",
+  builtin.live_grep,
   { desc = "Telescope: Live Grep", silent = true }
 )
+
+-- binds <leader>gf to a search for files tracked in git
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+
+
 
